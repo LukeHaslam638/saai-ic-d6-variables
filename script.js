fcee59482177a37ecd29cb91c0299195c8400169
LukeHaslam638 <Lukehaslam638@gmail.com>
2024-11-07 03:35:04 +0000

    Practiced Javascript Variables

---
+++ script.js
@@ -0,0 +1,9 @@
+let favoriteFood = "Probably Water"         
+console.log(favoriteFood)
+favoriteFood = "Antipods Water"
+console.log(favoriteFood)
+
+const favoriteHoliday = "thanksgiving"
+console.log(favoriteHoliday)
+
+console.log("value of favorite food", favoriteFood)
